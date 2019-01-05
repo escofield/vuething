2f6aeb4b5cad6e4c608fae43a5ae7cf5b4ee7efc
Erik <escofieldpublic@gmail.com>
2019-01-04 21:32:50 -0600

    test

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p class="bob">Some bob</p>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<style scoped>
+.bob {
+  color: var(--myYellow);
+}
+.home{
+  & .bob {
+    color: yellow;
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+var webpack = require('webpack');
+module.exports = {
+  configureWebpack: {
+    devtool: 'source-map',
+  },
+  css: {
+    sourceMap: true,
+  },
+  productionSourceMap: false,
+}
