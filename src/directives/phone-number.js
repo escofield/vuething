e4ab90359d77caaddd9b71293369164e954b0abe
Erik <escofieldpublic@gmail.com>
2019-01-14 20:32:45 -0600

    lint

--- src/directives/phone-number.js
+++ src/directives/phone-number.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import format from '../utils/format-phone'
 
 Vue.directive('phone-number', function(el, binding, vnode) {
-  const justNo = binding.value.replace(/\D/g,'')
-  if(!binding.oldValue || binding.value.length > binding.oldValue.length || justNo.length >= 11){
+  const justNo = binding.value.replace(/\D/g, '')
+  if (
+    !binding.oldValue ||
+    binding.value.length > binding.oldValue.length ||
+    justNo.length >= 11
+  ) {
     el.value = format(binding.value)
     vnode.context.$data[binding.expression] = el.value
   }
