cf5e9f33ff4ac129478cd94eb03c079ecd1a72fe
Erik <escofieldpublic@gmail.com>
2019-01-21 16:17:13 -0600

    remove zxcvbn due to size, this should probably be a server side function we can call as we type

--- src/directives/v-password.js
+++ src/directives/v-password.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import zxcvbn from 'zxcvbn'
 
 const classNames = [
   'vp-pass-no-score',
@@ -8,6 +7,20 @@ const classNames = [
   'vp-pass-safe',
   'vp-pass-strong',
 ]
+
+/* this should be zxcvbn but it's too big,  this should be a server side implimentatoin */
+const zxcvbn = (p) =>{
+  var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+  var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+  var enoughRegex = new RegExp("(?=.{10,}).*", "g");
+  var notEnoughRegex = new RegExp("(?=.{1,}).*", "g");
+
+  let score = notEnoughRegex.test(p) || 0 && 1
+  score += enoughRegex.test(p) || 0 && 1
+  score += mediumRegex.test(p) || 0 && + 1
+  score += strongRegex.test(p) || 0 && + 1
+  return {score}
+}
 
 Vue.directive('password', {
   update: function(el, binding, vnode) {
