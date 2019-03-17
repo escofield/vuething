6457d328e7bd71a53d0d83f22e53119d90107af8
Erik <escofield@pilgrimconsulting.com>
2019-03-17 00:41:45 -0500

    things

--- src/directives/v-loading.js
+++ src/directives/v-loading.js
@@ -4,11 +4,15 @@ Vue.directive('loading', {
   inserted: function(el) {
     if (el.className.indexOf('vp-loading') < 0) {
       el.innerHTML =
-        "<div class='vp-loading-mask'><div class='vp-spinner'></div></div>"
+        "<div class='vp-loading-mask' style='display: none'><div class='vp-spinner'></div></div>"
       el.classList.add('vp-loading')
     }
     if (el.parentNode.className.indexOf('vp-loading-container') < 0) {
       el.parentNode.classList.add('vp-loading-container')
     }
   },
+  update: function(el, binding){
+    const mask = el.querySelector('.vp-loading-mask')
+    mask.style.display = (binding && binding.value === true) ? 'block' : 'none';
+  }
 })
